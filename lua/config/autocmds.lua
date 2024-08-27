b5931f41183e85f1018b510fdd14f4486f8b55d2
Jiucheng Zang <zangjiucheng@gmail.com>
2024-08-27 18:58:12 -0400

    Update transparent background

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+{
+  "tokyonight.nvim",
+  opts = {
+        transparent = true,
+        styles = {
+           sidebars = "transparent",
+           floats = "transparent",
+        },
+  }
+}
