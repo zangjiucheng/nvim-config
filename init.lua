5f5c5cc78d09fc76c663256791a50a283956f777
Jiucheng Zang <git.jiucheng@gmail.com>
2025-02-11 10:11:51 -0500

    Update Config Structure

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- Set the runtime path for ocp-indent in Neovim (init.lua)
+vim.cmd([[set rtp^="/Users/jiucheng/.opam/5.2.0/share/ocp-indent/vim"]])
