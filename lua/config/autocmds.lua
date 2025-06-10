e4fd2c3c3505fd9c769672f213a06591e77fe0b4
Jiucheng Zang <git.jiucheng@gmail.com>
2025-06-09 22:40:55 -0400

    feat: update copilot plugin and increment version; add color column setting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("ColorColumn", { clear = true }),
+  desc = "Change ColorColumn highlight",
+  callback = function()
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2d2f34" }) -- subtle dark-grey stripe
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.api.nvim_set_option("clipboard","unnamedplus")
+local opt = vim.opt
+vim.g.autoformat = false
+opt.colorcolumn = "80,100"
