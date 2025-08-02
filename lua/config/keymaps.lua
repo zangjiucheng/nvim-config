d8bdbec9c05369844d977e62421a04928c710cc1
Jiucheng Zang <git.jiucheng@gmail.com>
2025-08-02 17:09:42 -0400

    feat: add auto-session and flash plugins with keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,8 +36,5 @@ vim.keymap.set(
 -- Toggle Copilot-Chat
 vim.api.nvim_set_keymap("n", "<leader>ac", "<cmd>CopilotChatToggle<cr>", { noremap = true, silent = true })
 
----- ToggleTerm
-
--- Toggle the terminal
-
-vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
